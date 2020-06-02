45829999f5edcb30639984bb4350f73eb114220c
georgy-kirilov <georgynk17@gmail.com>
2020-06-02 16:32:31 +0300

    Added solutions for Built in Funcs - problem 13

--- CourseLessons/BuiltInFunctions/Part-2-Queries-12-And-13.sql
+++ CourseLessons/BuiltInFunctions/Part-2-Queries-12-And-13.sql
@@ -1,9 +1,33 @@
 use [Geography];
 
--- Problem 12
+-- Problem 12 - type 1
 select CountryName, IsoCode
 	from Countries
 	where len(CountryName) - len(replace(CountryName, 'A', '')) >= 3
 	order by IsoCode;
 
--- Problem 13 => TODO
+-- Problem 12 - type 2
+select CountryName, IsoCode
+	from Countries
+	where CountryName like '%a%a%a%'
+	order by IsoCode;
+
+-- Problem 13 - type 1
+select 
+	PeakName, 
+	RiverName, 
+	lower(PeakName + substring(RiverName, 2, len(RiverName) - 1)) as [Mix] 
+	from Peaks as p, Rivers as r
+	where right(p.PeakName, 1) = left(r.RiverName, 1)
+	order by [Mix];
+
+-- Problem 13 - type 2
+select 
+	PeakName, 
+	RiverName, 
+	lower(PeakName + substring(RiverName, 2, len(RiverName) - 1)) as [Mix] 
+	from Peaks
+	inner join Rivers
+	on right(Peaks.PeakName, 1) = left(Rivers.RiverName, 1)
+	order by [Mix];
+	
